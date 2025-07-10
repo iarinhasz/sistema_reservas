753bc3402d8a646884d7314e38a6a8275d2df709
iarinhasz <iybp@cin.ufpe.br>
2025-07-10 14:24:29 -0300

    fazendo alteracoes nas features para teste e2e

--- cypress/e2e/login.js
+++
@@ -1,5 +0,0 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
-
-Given("a configuração de steps foi carregada", () => {
-  cy.log("SUCESSO! O arquivo de step definition foi encontrado e carregado!");
-});
