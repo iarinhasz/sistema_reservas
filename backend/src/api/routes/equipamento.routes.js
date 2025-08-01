211ab2cece57850feb7ed30012fcfb32a8e78e83
João Pedro Felix <jpfs2@cin.ufpe.br>
2025-08-01 19:33:45 -0300

    feat: refatora entidade Equipamento para arquitetura de classes

--- backend/src/api/routes/equipamento.routes.js
+++ backend/src/api/routes/equipamento.routes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
-import equipamentoController from '../controllers/equipamento.controller.js';
+import EquipamentoController from '../controllers/equipamento.controller.js'; // Importa a CLASSE
 import adminMiddleware from '../middlewares/admin.middleware.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 
-
 const router = Router();
-//somente admi
-router.post('/', authMiddleware, adminMiddleware, equipamentoController.create);
-router.patch('/:id', authMiddleware, adminMiddleware, equipamentoController.update);
-router.delete('/:id', authMiddleware, adminMiddleware, equipamentoController.delete);
 
-router.get('/', equipamentoController.listAll);
-router.get('/:id', equipamentoController.getById);
+const equipamentoController = new EquipamentoController();
 
+router.post('/', authMiddleware, adminMiddleware, equipamentoController.create.bind(equipamentoController));
+router.patch('/:id', authMiddleware, adminMiddleware, equipamentoController.update.bind(equipamentoController));
+router.delete('/:id', authMiddleware, adminMiddleware, equipamentoController.delete_.bind(equipamentoController));
+
+router.get('/', equipamentoController.listAll.bind(equipamentoController));
+router.get('/:id', equipamentoController.getById.bind(equipamentoController));
 
 export default router;
