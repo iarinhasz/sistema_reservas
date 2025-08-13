74b2499c03f89d8c580287ec2cc76bfbcd075565
iarinhasz <iybp@cin.ufpe.br>
2025-08-12 21:41:30 -0300

    arruma botoes de ambiente

--- frontend/src/pages/public/PublicAmbienteDetalhesPage.jsx
+++ frontend/src/pages/public/PublicAmbienteDetalhesPage.jsx
@@ -4,7 +4,6 @@ import api from '../../services/api';
 import AgendaAmbiente from '../../components/shared/AgendaAmbiente';
 import { useAuth} from '../../context/AuthContext';
 import ReservarModal from '../../components/shared/ReservarModal';
-// CORREÇÃO: Importa os estilos do layout do utilizador logado
 import styles from '../../components/layout/UserLayout.module.css'; 
 import EquipamentosList from '../../components/shared/EquipamentoList'; 
 import Button from '../../components/shared/Button'; 
@@ -73,15 +72,17 @@ const PublicAmbienteDetalhesPage = () => {
                     <h1>{ambiente.identificacao}</h1>
                     <p>Veja a agenda de horários e solicite sua reserva.</p>
                 </div>
-                <Button as={Link} to={`/${user.tipo}/minhas-reservas?recursoId=${id}&recursoTipo=ambiente`} variant="primary">
-                    Minhas Reservas
-                </Button>
-                    {user.tipo === 'professor' && (
-                <Button onClick={handleOpenReserveModal} variant="secondary">
-                    + Fazer Nova Reserva
-                </Button>
-                    )}
-                <Button as={Link} to={homePath} variant="cancel">Voltar para Início</Button>
+                <div className={styles.headerActions}> 
+                    <Button as={Link} to={`/${user.tipo}/minhas-reservas?recursoId=${id}&recursoTipo=ambiente`} variant="primary">
+                        Minhas Reservas
+                    </Button>
+                        {user.tipo === 'professor' && (
+                    <Button onClick={handleOpenReserveModal} variant="secondary">
+                        + Fazer Nova Reserva
+                    </Button>
+                        )}
+                    <Button as={Link} to={homePath} variant="cancel">Voltar para Início</Button>
+                </div>
             </div>
             
             {user && (
